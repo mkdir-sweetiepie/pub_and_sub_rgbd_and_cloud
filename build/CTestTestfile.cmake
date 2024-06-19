40260e010ecd6429329b4aaecd25f2aa49cb2185
Hong Jihyeon <hong091788@naver.com>
2024-06-19 19:09:31 +0900

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/hyeon/catkin_ws/src/ros_pub_and_sub_rgbd_and_cloud
+# Build directory: /home/hyeon/catkin_ws/src/ros_pub_and_sub_rgbd_and_cloud/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/hyeon/catkin_ws/src/ros_pub_and_sub_rgbd_and_cloud/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/hyeon/catkin_ws/src/ros_pub_and_sub_rgbd_and_cloud/build/devel/lib:$LD_LIBRARY_PATH"
+export PATH='/opt/ros/noetic/bin:/home/hyeon/.local/bin:/home/hyeon/.rbenv/shims:/home/hyeon/.rbenv/bin:/home/hyeon/gems/bin:/home/hyeon/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/hyeon/catkin_ws/src/ros_pub_and_sub_rgbd_and_cloud/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/hyeon/catkin_ws/src/ros_pub_and_sub_rgbd_and_cloud/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/hyeon/catkin_ws/src/ros_pub_and_sub_rgbd_and_cloud/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/hyeon/catkin_ws/src/ros_pub_and_sub_rgbd_and_cloud:$ROS_PACKAGE_PATH"
